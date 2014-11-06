7eb7a2a7b275cac4824eca68b952ff3b6254cdad
ikouoh <i.kouoh@gmail.com>
2014-11-06 22:47:28 +0100

    set database

--- application/config/routes.php
+++ application/config/routes.php
@@ -64,7 +64,6 @@ $route['ajax/LienMort'] = "ajax_controller/LienMort";
 * routes back
 */
 $route['beyond'] = "beyond/beyond_controller";
-$route['beyond/setdatabase'] = "beyond/beyond_controller/set_database";
 
 $route['beyond/artiste'] = "beyond/artiste_controller/index";
 $route['beyond/artiste/(:num)'] = "beyond/artiste_controller/index/$1";
--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -20,5 +20,12 @@ class Welcome extends Front_Controller {
 		
 	}
 
-	
+	public function set_database(){
+		$em = $this->doctrine->em;
+		$schemaTool = new \Doctrine\ORM\Tools\SchemaTool($em);
+		$classes = $em->getMetadataFactory()->getAllMetadata();
+		//$schemaTool->dropSchema($classes);
+		//$schemaTool->createSchema($classes);
+		$schemaTool->updateSchema($classes);
+	}
 }
